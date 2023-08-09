92491284aa44ac67a4e9f870e14ea3a3dde073e3
LEvans-Nelmes <l.evans.nelmes@gmail.com>
2023-08-09 22:01:52 +0100

    changes: renaming files loading all at once

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-const carBert = {
-    fileName:"Bert",
+const carBeetle = {
+    fileName:"Beetle",
     CarPositionOver:11,
     CarPositionDown:59,
     ArchPositionOver:16,
@@ -11,8 +11,8 @@ const carBert = {
     RoofRefDown:55
 };
 
-const car3cube = {
-    fileName:"3cube",
+const carT3_squareback = {
+    fileName:"T3_squareback",
     CarPositionOver:11,
     CarPositionDown:61,
     ArchPositionOver:19,
@@ -23,9 +23,21 @@ const car3cube = {
     RoofRefDown:57
 };
 
-const wheelBlackFechs = {
-    filename:"BlackFechs",
-    size:12
+var currentDisplay = {
+    id:01,
+    body:carBeetle,
+    baseColour:"#008000",
+    secondColour:"",
+    twoTone:0,
+    darkColour:-0.25,
+    lightColour:0.25,
+    wheelName:"BlackFechs",
+    wheelSize:12,
+    frontTyreSize:18,
+    backTyreSize:18,
+    bodyDrop:1,
+    rack:"BowRack",
+    rackAccessory:"LongSurf",
 };
 
 
@@ -51,46 +63,92 @@ window.onclick = function(event) {
         }
         }
     }
-    if (event.target.matches('.BertButton')) {
-        draw(carBert)
+    if (event.target.matches('.BeetleButton')) {
+        currentDisplay.body = carBeetle
+        draw()
     }
-    if (event.target.matches('._3cubeButton')) {
-        draw(car3cube)
+    if (event.target.matches('.T3_squarebackButton')) {
+        currentDisplay.body = carT3_squareback
+        draw()
     }
 }
 
-function draw(carSelection = carBert) {
+function loadImageToArray(imageIn) {
+ 
+    const hiddenCanvas = document.getElementById("hiddenCanvas").getContext("2d");
+
+    
+
+
+
+
+
+
+
+}
+
+function draw() {
     const floorline = 100;
-    const canvas = document.getElementById("tutorial").getContext("2d");
+    const visibleCanvas = document.getElementById("mainCanvas").getContext("2d");
+    const hiddenCanvas = document.getElementById("hiddenCanvas").getContext("2d");
     const body = new Image(); 
     const frontWheel = new Image(); 
     const backWheel = new Image(); 
+    const arches = new Image();
 
-    const carChoice = carSelection
-    const wheelChoice = wheelBlackFechs
+    var imagesToLoad = [];
+    var imagesLoaded = [];
+    var imageCount = 0;
 
-    canvas.clearRect(0,0,128,128);
 
-    body.onload = () => {
-        canvas.drawImage(body,carChoice.CarPositionOver,carChoice.CarPositionDown);
-    };
+    visibleCanvas.clearRect(0,0,128,128);
+    visibleCanvas.offscreenCanvas = document.createElement("canvas");
+    visibleCanvas.offscreenCanvas.height = visibleCanvas.height;
+    visibleCanvas.offscreenCanvas.height = visibleCanvas.height;
 
-    frontWheel.onload = () => {
-        canvas.drawImage(frontWheel,
-            carChoice.FrontWheelOver - wheelChoice.size/2,
-            floorline - wheelChoice.size - (16 - wheelChoice.size)/2);
-    }
-    backWheel.onload = () => {
-        canvas.drawImage(backWheel,
-            carChoice.BackWheelOver - wheelChoice.size/2 ,
-            floorline - wheelChoice.size - (16 - wheelChoice.size)/2);
-    }
 
-    body.src = "ImageLibrary\\Car\\"+carChoice.fileName+"\\"+carChoice.fileName+".body.png"; // Set source path
-    frontWheel.src = "ImageLibrary\\Wheels\\Wheel."+wheelChoice.size+"."+wheelChoice.filename+".png"
+    body.src = "ImageLibrary\\Car\\"+currentDisplay.body.fileName+"\\"+currentDisplay.body.fileName+".body.png"; // Set source path
+    imagesToLoad.push(body);
+    frontWheel.src = "ImageLibrary\\Wheels\\Wheel."+currentDisplay.wheelSize+"."+currentDisplay.wheelName+".png"
+    imagesToLoad.push(frontWheel);
     backWheel.src = frontWheel.src
+    imagesToLoad.push(backWheel);
+    arches.src = "ImageLibrary\\Car\\"+currentDisplay.body.fileName+"\\"+currentDisplay.body.fileName+".arches.png";
+    imagesToLoad.push(arches);
+
+
+
+
+
+
+
+
+    imagesToLoad.forEach(image =>{
+        image.onload = ()=>{ 
+            imageCount +=1;
+            if(imageCount == imagesToLoad.length){
+
+                visibleCanvas.drawImage(arches,currentDisplay.body.ArchPositionOver,currentDisplay.body.ArchPositionDown+currentDisplay.bodyDrop);
+
+                visibleCanvas.drawImage(body,currentDisplay.body.CarPositionOver,currentDisplay.body.CarPositionDown+currentDisplay.bodyDrop);
+
+                visibleCanvas.drawImage(frontWheel,
+                    currentDisplay.body.FrontWheelOver - currentDisplay.wheelSize/2,
+                    floorline - currentDisplay.wheelSize - (currentDisplay.frontTyreSize - currentDisplay.wheelSize)/2);
+
+                    visibleCanvas.drawImage(backWheel,
+                    currentDisplay.body.BackWheelOver - currentDisplay.wheelSize/2 ,
+                    floorline - currentDisplay.wheelSize - (currentDisplay.frontTyreSize - currentDisplay.wheelSize)/2);
+
+            }
+        }
+
+
+
+    })
+
     
 }
-draw(carBert);
+draw();
 
 
